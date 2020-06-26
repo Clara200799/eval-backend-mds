635995457aa11b092e2ea091581c2ce1d1f94005
Clara200799 <62375518+Clara200799@users.noreply.github.com>
2020-06-26 09:50:51 +0200

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -5,16 +5,19 @@
     <title>Formuliare Application</title>
   </head>
   <body>
+  <?php
+  echo "<h2> Formuliare </h2>";
+          
 <form method="get" action="formulaire.php"> 
- 
 <p>
 <input type="text" name="cfname"  value="cfname"/>
 <input type="text" ville="cfville"  value="cfville"/>
 <input type="text" téléphone="cftéléphone" value="cftéléphone" />
 <input type="text" mail="cfmail"  value="cfmail" />
 </p>
- 
 </form>
+?>
+  </body>
 </html>
 Avec la méthodee get, les données transiteront par l'URL. On pourra les récupérer grâce à $_GET. En revanche cette méthode est peu utilisée car on ne peut pas envoyer beaucoup d'informations dans l'URL. Le action sert à envoyée les informations collectées sur une autre page. Ici elle reste sur la page du formulaire. 
 Ensuite, les balises type correspondent à un champs et les balise value permettent d'afficher les nom dans les champs. 
