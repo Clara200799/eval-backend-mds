8946c4388d3fd17b7de7755d69ae9eb8b33f6daf
Clara200799 <62375518+Clara200799@users.noreply.github.com>
2020-06-26 10:28:34 +0200

    Update formulaire.php
    les donées suplémentaires sont ajoutées au formulaire, mais il me manque l'echo

--- formulaire.php
+++ formulaire.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Formuliare Application</title>
-  </head>
-  <body>
+
   <?php
   echo "<h2> Formuliare </h2>";
           
@@ -14,10 +8,19 @@
 <input type="text" ville="cfville"  value="cfville"/>
 <input type="text" téléphone="cftéléphone" value="cftéléphone" />
 <input type="text" mail="cfmail"  value="cfmail" />
-</p>
+<select name="type de projet">
+    <option value="wordpress">Choix 1</option>
+    <option value="vue.js">Choix 2</option>
+</select>
+Options du projet
+<input type="checkbox" name="formulaire de contact avancé" id="formulaire de contact avancé" /> 
+<input type="checkbox" name="adaptations mobile spécifiques" id="adaptations mobile spécifiques" /> 
+<input type="checkbox" name="design personnalisé" id="design personnalisé" /> 
 </form>
 ?>
-  </body>
-</html>
-Avec la méthodee get, les données transiteront par l'URL. On pourra les récupérer grâce à $_GET. En revanche cette méthode est peu utilisée car on ne peut pas envoyer beaucoup d'informations dans l'URL. Le action sert à envoyée les informations collectées sur une autre page. Ici elle reste sur la page du formulaire. 
+
+Avec la méthodee get, les données transiteront par l'URL. En revanche cette méthode est peu utilisée car on ne peut pas envoyer beaucoup d'informations dans l'URL. 
+Le action sert à envoyée les informations collectées sur une autre page. Ici elle reste sur la page du formulaire. 
 Ensuite, les balises type correspondent à un champs et les balise value permettent d'afficher les nom dans les champs. 
+La balise select name permet de faire un choix entre plusieurs propositions. Ici WP ou vue. 
+La balise checkbox permet de crée des cases à cochées. Ici nous avons 3 cases à cochées. 
